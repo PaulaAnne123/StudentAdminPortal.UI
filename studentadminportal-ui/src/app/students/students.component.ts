553775fee734f95630c4c5350ae903177fdb2a0a
Paula McGrath <Paula.McGrath@ct.gov>
2022-03-06 19:13:12 -0500

    Updates

--- studentadminportal-ui/src/app/students/students.component.ts
+++ studentadminportal-ui/src/app/students/students.component.ts
@@ -1,4 +1,7 @@
+import { ViewChild } from '@angular/core';
 import { Component, OnInit } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 import { Student } from '../models/ui-models/student.model';
 import { StudentService } from './student.service';
@@ -14,7 +17,9 @@ export class StudentsComponent implements OnInit {
   displayedColumns: string[] = ['firstName', 'lastName', 'dateOfBirth', 'email', 'mobile',
     'gender'];
   dataSource: MatTableDataSource<Student> = new MatTableDataSource<Student>();
-
+  @ViewChild(MatPaginator) matPaginator!: MatPaginator;
+  @ViewChild(MatSort) matSort!: MatSort;
+  filterString = '';
 
   constructor(private studentService: StudentService) { }
 
@@ -25,12 +30,25 @@ export class StudentsComponent implements OnInit {
       (successResponse) => {
         this.students = successResponse;
         this.dataSource = new MatTableDataSource<Student>(this.students);
+
+        if (this.matPaginator) {
+          this.dataSource.paginator = this.matPaginator;
+        }
+
+        if (this.matSort) {
+          this.dataSource.sort = this.matSort;
+        }
+
       },
       (errorResponse) => {
         console.log(errorResponse);
       }
     );
 
+  }
+
+  filterStudents() {
+    this.dataSource.filter = this.filterString.trim().toLowerCase();
   }
 
 }
