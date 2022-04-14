4f364a86e72eddacffde08fd51028c6146d63920
Paula McGrath <Paula.McGrath@ct.gov>
2022-04-14 12:03:09 -0400

    Check in

--- studentadminportal-ui/src/app/students/view-student/view-student.component.ts
+++ studentadminportal-ui/src/app/students/view-student/view-student.component.ts
@@ -33,6 +33,8 @@ export class ViewStudentComponent implements OnInit {
       postalAddress: ''
     }
   };
+  isNewStudent = false;
+  header = '';
 
   constructor(private readonly studentService: StudentService,
     private readonly route: ActivatedRoute, private readonly genderService: GenderService,
@@ -44,12 +46,25 @@ export class ViewStudentComponent implements OnInit {
         this.studentId = params.get('id');
 
         if (this.studentId) {
-          this.studentService.getStudent(this.studentId)
-            .subscribe(
-              (successResponse) => {
-                this.student = successResponse;
-              }
-            );
+
+          if (this.studentId.toLowerCase() === 'Add'.toLowerCase()) {
+            // New Student functionality
+            this.isNewStudent = true;
+            this.header = 'Add New Student';
+          }
+          else {
+             //Existing student functionality
+            this.isNewStudent = false;
+            this.header = 'Edit Student';
+
+            this.studentService.getStudent(this.studentId)
+              .subscribe(
+                (successResponse) => {
+                  this.student = successResponse;
+                }
+              );
+
+          }
 
           this.genderService.getGenderList()
             .subscribe(
@@ -57,10 +72,7 @@ export class ViewStudentComponent implements OnInit {
                 this.genderList = successResponse;
               }
             );
-        }
-
-      
-
+        } 
       }
     )
   }
@@ -98,6 +110,26 @@ export class ViewStudentComponent implements OnInit {
           // Log It
         }
       );
+  }
+
+  onAdd(): void {
+    this.studentService.addStudent(this.student)
+      .subscribe(
+        (successResponse) => {
+          this.snackbar.open('Student added successfully', undefined, {
+            duration: 2000
+          });
+
+          setTimeout(() => {
+            this.router.navigateByUrl(`students/${successResponse.id}`);
+          }, 2000);
+
+        },
+        (errorResponse) => {
+          // Log It
+        }
+      );
+
   }
 
 }
